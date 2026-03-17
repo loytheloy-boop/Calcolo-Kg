16028bffd040ce820ce003c53ee664efb79dab06
loytheloy-boop <loytheloy@gmail.com>
2026-03-17 18:43:03 +0100

    Change cached assets to external URLs
    Updated cache assets to use external URLs.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("kg-app").then(cache => {
-            return cache.addAll(["./", "index.html", "style.css", "app.js"]);
+            return cache.addAll([
+                "https://loytheloy-boop.github.io/Calcolo-Kg/",
+                "https://loytheloy-boop.github.io/Calcolo-Kg/index.html",
+                "https://loytheloy-boop.github.io/Calcolo-Kg/style.css",
+                "https://loytheloy-boop.github.io/Calcolo-Kg/app.js"
+            ]);
         })
-    );
-});
-
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
     );
 });
