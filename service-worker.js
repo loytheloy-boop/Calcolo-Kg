6aa02bafd1c78af121b9e114c1594eab44b8d79a
loytheloy-boop <loytheloy@gmail.com>
2026-03-17 18:43:21 +0100

    Add fetch event listener to service worker

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,9 @@ self.addEventListener("install", e => {
         })
     );
 });
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
